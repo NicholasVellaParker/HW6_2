1c43c0271bc147f9ad59c42913ee8a4ab3011fd8
NicholasVellaParker <nicholas.vella.parker@okstate.edu>
2026-04-13 19:41:45 -0500

    Update Fluid.py

--- Fluid.py
+++ Fluid.py
@@ -1,16 +1,16 @@
 #region class definitions
 class Fluid():
     #region constructor
-    def __init__(self, mu=0.00089, rho=1000):
+    def __init__(self, mu=0.0000205, rho=1.93):
         '''
         default properties are for water
-        :param mu: dynamic viscosity in Pa*s -> (kg*m/s^2)*(s/m^2) -> kg/(m*s)
-        :param rho: density in kg/m^3
+        :param mu: dynamic viscosity in psi*s -> (lbf*s)/ft^2
+        :param rho: density in slug/ft^3
         '''
         #region attributes
         self.mu= mu# $JES MISSING CODE$  # simply make a copy of the value in the argument as a class property
         self.rho= rho # $JES MISSING CODE$  # simply make a copy of the value in the argument as a class property
-        self.nu= mu/rho #JES MISSING CODE$ # calculate the kinematic viscosity in units of m^2/s
+        self.nu= mu/rho #JES MISSING CODE$ # calculate the kinematic viscosity in units of ft^2/s
         #endregion
     #endregion
 #endregion
